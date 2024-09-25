8c63120be298437c133b5b244428dc03d3f93aca
Koushik Karmakar <106049540+Karmakarkoushik32@users.noreply.github.com>
2024-09-26 01:47:26 +0530

    Update list_devices.py

--- gui/model/list_devices.py
+++ gui/model/list_devices.py
@@ -2,4 +2,4 @@ import torch
 
 def list_devices():
     num_gpus = torch.cuda.device_count()
-    return ['cpu',*[torch.cuda.get_device_name(i) for i in range(num_gpus)]]
+    return ['cpu',*[f"cuda:{device_id}|{torch.cuda.get_device_name(device_id)}" for device_id in range(num_gpus)]]
